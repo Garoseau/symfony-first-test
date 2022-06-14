1d074cfc3ec87ae517f9aba8c4934e74e0455c4e
Garoseau <Garoseau.Pro@gmail.com>
2022-06-14 12:34:00 +0200

    m

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,8 +1,11 @@
 <?php 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class DefaultController{
 
@@ -14,10 +17,35 @@ class DefaultController{
 	}
 
 	/**
-	 * @Route("/services")
+	 * @Route("/services", name="app_services")
 	 */
 	public function services(){
-		return new Response("<ul><li>Informatique</li><li>Gestion</li></ul>");
+		return new Response("<ul>
+												 <li>Informatique</li>
+												 <li>Gestion</li>
+												 </ul>");
 	}
+
+	/**
+	 * @Route("/product/{id?}", name="app_product_id")
+	 */
+	public function getShoes(?int $id, SessionInterface $session ){
+		// $session->set('numero', $id);
+		dd($session->get('numero'));
+		return new Response("La chaussure n° $id" );
+	}
+
+	/**
+	 * @Route("/post", name="app_post")
+	 */
+	public function articles(Request $request, SessionInterface $session){
+		// dd($request->attributes->get('_route'));
+		// dd($request);
+		dd($session->get('numero')); 
+		return new Response("");
+	}
+
+
+
 
 }
